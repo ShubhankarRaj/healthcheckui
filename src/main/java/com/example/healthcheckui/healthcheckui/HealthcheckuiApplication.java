dea7e1447767d6e40e1d3c31108fde437dce89f2
Shubhankar.Raj <Shubhankar.Raj@go-mmt.com>
2018-03-20 15:18:00 +0530

    Adding src and pom file to git

---
+++ src/main/java/com/example/healthcheckui/healthcheckui/HealthcheckuiApplication.java
@@ -0,0 +1,12 @@
+package com.example.healthcheckui.healthcheckui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcheckuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthcheckuiApplication.class, args);
+	}
+}
