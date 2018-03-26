cf581682161ac3ec9d78fefccfc8571d37e45698
Shubhankar.Raj <Shubhankar.Raj@go-mmt.com>
2018-03-26 11:26:32 +0530

    Modifying the Title of the Application to App HealthCheck Monitor from Healthcheck Monitor

--- src/healthcheckfrontend/src/app/app.component.ts
+++ src/healthcheckfrontend/src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Healthcheck Monitor';
+  title = 'App HealthCheck Monitor';
    isClassVisible: boolean = false;
 }
